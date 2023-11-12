134b8804658721ced44a97b3e8d43aca79a79ad0
ghthbl <147247605+ghthbl@users.noreply.github.com>
2023-11-12 23:45:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ document.getElementById('inputForm').addEventListener('submit', function(e) {
         console.error('Error:', error);
     });
 });
+
+document.getElementById('loadIframe').addEventListener('click', function() {
+    var iframeHTML = '<iframe src="https://scratch.mit.edu/projects/923076276/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>';
+    document.getElementById('iframeContainer').innerHTML = iframeHTML;
+    document.getElementById('iframeContainer').style.display = 'block';
+    this.style.display = 'none';
+    document.getElementById('closeIframe').style.display = 'block';
+});
+
+document.getElementById('closeIframe').addEventListener('click', function() {
+    document.getElementById('iframeContainer').innerHTML = '';
+    document.getElementById('iframeContainer').style.display = 'none';
+    this.style.display = 'none';
+    document.getElementById('loadIframe').style.display = 'block';
+});
